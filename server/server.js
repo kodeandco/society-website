365eff8f764060331838c70c0a2ffeab1a15e5eb
maanvi21 <maanvishadakshari21@gmail.com>
2025-08-17 09:04:44 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./db');
 const loginRoute= require('./routes/login_route');
 const signupRoute = require('./routes/signup_route');
 const tenderRoute=require('./routes/tenders_route');
+const downloadRoute = require('./routes/download_route');
 // Connect to the database
 connectDB();
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/login', loginRoute);
 app.use('/signup', signupRoute);
 app.use('/tenders', tenderRoute);
+app.use('/tenders/download', downloadRoute);
 app.get('/', (req, res) => {
     res.send('API running');
 });
